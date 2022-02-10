f89b461daf5056aed762bbbb46f1631bf42c133d
Surendrasuni1 <98877624+Surendrasuni1@users.noreply.github.com>
2022-02-10 18:51:21 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,6 +1,6 @@
 import boto3
 
-client = boto3.client('ec2',region_name='us-west-2',aws_access_key_id='AKIAQA3SUJXDWHSDUUVY',aws_secret_access_key='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')
+client = boto3.client('ec2',region_name='us-west-2',aws_access_key_id='aaaaaaaaaaaaaaaaaaaaaa',aws_secret_access_key='bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
 response =client.describe_instances()
 print(response)
 for data in response['ResponseMetadata']:
