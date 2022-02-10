fd89064fe2201051c2868800200622ac161efef7
Surendrasuni1 <98877624+Surendrasuni1@users.noreply.github.com>
2022-02-10 19:06:23 +0530

    Update listss3.py

--- listss3.py
+++ listss3.py
@@ -1,6 +1,6 @@
 import boto3
 
-client =boto3.client('s3',aws_access_key_id='AKIAQA3SUJXDWHSDUUVY',aws_secret_access_key='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')
+client =boto3.client('s3',aws_access_key_id='aaaaaaaaaaaaaaa',aws_secret_access_key='bbbbbbbbbbbbbbbbbbbbbbb')
 response=client.list_buckets()
 print(response['ResponseMetadata'])
 #
