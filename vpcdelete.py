0e93509df4ccd7c4167e4fbe01522da2a9b95985
Surendrasuni1 <98877624+Surendrasuni1@users.noreply.github.com>
2022-02-10 19:10:05 +0530

    Update vpcdelete.py

--- vpcdelete.py
+++ vpcdelete.py
@@ -1,6 +1,6 @@
 import boto3
 
-client =boto3.client('ec2',region_name='us-west-2',aws_access_key_id='AKIAQA3SUJXDWHSDUUVY',aws_secret_access_key='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')
+client =boto3.client('ec2',region_name='us-west-2',aws_access_key_id='pppppppppppppppY',aws_secret_access_key='oooooooooooooooooooooooooooooo')
 
 #igw_detach_response=client.detach_internet_gateway(InternetGatewayId='igw-0c7aa6c14d8d5c7d5',VpcId='vpc-04ba23e9278acfcd0')
 #print(igw_detach_response)
