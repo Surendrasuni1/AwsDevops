f92c66527dbb7fdbc00c2efb4c952f5680829bec
Surendrasuni1 <98877624+Surendrasuni1@users.noreply.github.com>
2022-02-10 19:08:02 +0530

    Update vpc.py

--- vpc.py
+++ vpc.py
@@ -1,6 +1,6 @@
 import boto3
 
-client = boto3.client('ec2',region_name='us-west-2',aws_access_key_id='AKIAQA3SUJXDWHSDUUVY',aws_secret_access_key='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')
+client = boto3.client('ec2',region_name='us-west-2',aws_access_key_id='oooooooooooooooooooooooo',aws_secret_access_key='kkkkkkkkkkkkkkkkkkkkkkkkk7')
 
 def CreateVPC():
     response = client.create_vpc(
